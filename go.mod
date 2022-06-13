3b5d4d5aea27ce5cd8cba6718ab8d9ef8652b37a
Christian Dupuis <cd@atomist.com>
2022-06-13 12:12:51 +0200

    Add commit signature transaction

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module go-sample-skill
 
 go 1.18
 
-require github.com/atomist-skills/go-skill v0.0.0-20220612101631-51f3747dba4d
+require (
+	github.com/atomist-skills/go-skill v0.0.0-20220612102819-3fead61f6e37
+	github.com/google/go-github/v45 v45.1.0
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3
+)
 
 require (
 	cloud.google.com/go v0.102.0 // indirect
@@ -13,8 +17,10 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
 	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
 	golang.org/x/oauth2 v0.0.0-20220608161450-d0670ef3b1eb // indirect
 	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f // indirect
@@ -25,5 +31,4 @@ require (
 	google.golang.org/genproto v0.0.0-20220608133413-ed9918b62aac // indirect
 	google.golang.org/grpc v1.47.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
-	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- handlers.go
+++ handlers.go
@@ -17,8 +17,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/atomist-skills/go-skill"
+	"github.com/google/go-github/v45/github"
 	"olympos.io/encoding/edn"
 )
 
@@ -30,12 +32,14 @@ type GitCommitAuthor struct {
 type GitOrg struct {
 	Name              string `json:"git.org/name"`
 	InstallationToken string `json:"github.org/installation-token"`
+	Url               string `json:":git.provider/url"`
 }
 
 type GitRepo struct {
 	Name          string `json:"git.repo/name"`
 	DefaultBranch string `json:"git.repo/default-branch"`
 	Org           GitOrg `json:"git.repo/org"`
+	SourceId      string `json:"git.repo/source-id"`
 }
 
 type GitCommit struct {
@@ -45,27 +49,77 @@ type GitCommit struct {
 	Repo    GitRepo         `json:"git.commit/repo"`
 }
 
-type GitCommitMarker struct {
+type GitRepoEntity struct {
 	EntityType edn.Keyword `edn:"schema/entity-type"`
-	Marker     string      `edn:"git.commit/marker"`
+	Entity     string      `edn:"schema/entity"`
+	SourceId   string      `edn:"git.repo/source-id"`
+	Url        string      `edn:"git.provider/url"`
 }
+
+type GitCommitEntity struct {
+	EntityType edn.Keyword `edn:"schema/entity-type"`
+	Entity     string      `edn:"schema/entity"`
+	Sha        string      `edn:"git.commit/sha"`
+	Repo       string      `edn:"git.commit/repo"`
+	Url        string      `edn:"git.provider/url"`
+}
+
+type GitCommitSignatureEntity struct {
+	EntityType edn.Keyword `edn:"schema/entity-type"`
+	Entity     string      `edn:"schema/entity"`
+	Commit     string      `edn:"git.commit.signature/commit"`
+	Signature  string      `edn:"git.commit.signature/signature"`
+	Reason     string      `edn:"git.commit.signature/reason"`
+	Verified   edn.Keyword `edn:"git.commit.signature/verified"`
+}
+
+const (
+	Verified    edn.Keyword = "git.commit.signature/VERIFIED"
+	NotVerified             = "git.commit.signature/NOT_VERIFIED"
+)
 
 func PrintCommit(ctx skill.EventContext) skill.Status {
 
 	for _, e := range ctx.Event.Subscription.Result {
 		commit := skill.Decode[GitCommit](e[0])
-		ctx.Log.Printf("Seen commit %s %s", commit.Sha, commit.Message)
-	}
 
-	/*entities := []GitCommitMarker{{
-		EntityType: "git/commit",
-		Marker:     "test",
-	}}
-	s := make([]interface{}, len(entities))
-	for i, v := range entities {
-		s[i] = v
+		client := github.NewClient(nil)
+		gitCommit, _, err := client.Repositories.GetCommit(context.Background(), commit.Repo.Org.Name, commit.Repo.Name, commit.Sha, nil)
+		if err != nil {
+			fmt.Println(err)
+			return skill.Status{
+				Code:   1,
+				Reason: fmt.Sprintf("Failed to obtain commit for %s", commit.Sha),
+			}
+		}
+
+		var verified edn.Keyword
+		if *gitCommit.Commit.Verification.Verified {
+			verified = Verified
+		} else {
+			verified = NotVerified
+		}
+
+		ctx.Transact([]any{GitRepoEntity{
+			EntityType: "git/repo",
+			Entity:     "$repo",
+			SourceId:   commit.Repo.SourceId,
+			Url:        commit.Repo.Org.Url,
+		}, GitCommitEntity{
+			EntityType: "git/commit",
+			Entity:     "$commit",
+			Sha:        commit.Sha,
+			Repo:       "$repo",
+			Url:        commit.Repo.Org.Url,
+		}, GitCommitSignatureEntity{
+			Commit:    "$commit",
+			Signature: *gitCommit.Commit.Verification.Signature,
+			Verified:  verified,
+			Reason:    *gitCommit.Commit.Verification.Reason,
+		}})
+
+		ctx.Log.Printf("Transacted commit signature for %s", commit.Sha)
 	}
-	ctx.Transact(s)*/
 
 	return skill.Status{
 		Code:   0,
